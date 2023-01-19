5e7ab324ee62867fcca3b3681aa5eeb6cf540cd5
root <root@ip-172-31-0-165.ap-northeast-2.compute.internal>
2023-01-19 08:02:09 +0000

    nginx 443, with installed wordpress

---
+++ src/nginx/script/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir /etc/nginx/certificate
+cd /etc/nginx/certificate
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key \
+	-subj "/C=KR/ST=test_state/L=test_loc/O=test_org/OU=IT Department/CN=54.180.5.25"
+
+service nginx restart
+
+bash
+
+#exec "$@"
