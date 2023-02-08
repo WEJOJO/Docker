1d9e1e2a8d4b69ebee39088611a291272abf0444
root <root@ip-172-31-0-165.ap-northeast-2.compute.internal>
2023-02-08 09:50:58 +0000

    add user, url=54.180.5.25

--- src/wordpress/script/script.sh
+++ src/wordpress/script/script.sh
@@ -14,5 +14,15 @@ touch /run/php/php7.3-fpm.pid
 cd /tmp
 
 mv wp-config.php /var/www/html
+cd /var/www/html
+apt-get update
+apt-get install -y curl
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core install --url=54.180.5.25 --title=nhwangtest --admin_user=supervisor --admin_password=testpassword --admin_email=hni1124@naver.com --allow-root
+
+wp user create nhwang skadlr650@gmail.com --user_pass=mypassword --allow-root
 
 bash
