024ea1164f15407ac11fbaf32d1b8623d3886240
root <root@ip-172-31-0-165.ap-northeast-2.compute.internal>
2023-02-14 09:39:19 +0000

    db env passed

--- src/mariadb/script/script.sh
+++ src/mariadb/script/script.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-#service mysql start
+service mysql start
 #cd /usr/sbin/
 #mysqld
+
+mysql -uroot <<- EOSQL
+	CREATE DATABASE ${WORDPRESS_DB_NAME};
+	CREATE USER '${WORDPRESS_DB_USER}'@'${WORDPRESS_DB_USER}' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
+	FLUSH PRIVILEGES;
+EOSQL
+
+mysqladmin -uroot -p${MYSQL_PASSWORD} shutdown
 
 exec "$@"
