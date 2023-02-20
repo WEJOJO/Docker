333f71e020d21b5e0d595206c104f21f2d9fa1b6
root <root@ip-172-31-0-165.ap-northeast-2.compute.internal>
2023-02-20 05:06:56 +0000

    nginx_foregrd

--- src/nginx/script/script.sh
+++ src/nginx/script/script.sh
@@ -5,8 +5,4 @@ cd /etc/nginx/certificate
 openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key \
 	-subj "/C=KR/ST=test_state/L=test_loc/O=test_org/OU=IT Department/CN=54.180.5.25"
 
-service nginx restart
-
-bash
-
-#exec "$@"
+exec "$@"
